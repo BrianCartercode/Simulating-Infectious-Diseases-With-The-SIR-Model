1ea6cb1969455be912fc0c6f2b12802407ce6c7e
Brian <bryancart6@gmail.com>
2026-04-24 23:00:02 +0100

    added if statement to make sure total population = 100%

--- SIR Models/SIR.py
+++ SIR Models/SIR.py
@@ -11,17 +11,20 @@ def SIR_model(y, t, beta, gamma):
     return [dSdt, dIdt, dRdt]
 
 
-"""
-Initial conditions (such as S0, I0, and R0) are not to be random but I hardcoded them with specific values. These choices are typically made based on the characteristics of the disease being modeled and the context of the simulation. Initial condition are set such that S0 = 99%, which indicates the proportion of susceptible individuals when the simulation starts. I0 is set to 1%, which indicates proportion of infected individuals to be 1% when the simulation starts. R0 is set to 0% which is expected that there are are no recovered individuals when the simulations start.
-"""
-S0 = 0.99
+## initial conditions
+S0 = 0.99   ##total must add up to 100% 
 I0 = 0.01
-R0 = 0.32
+R0 = 0.0
 y0 = [S0, I0, R0]
+
+
+if S0 + I0 + R0 != 1.0:
+    raise ValueError("Initial conditions must sum to 1.0 (100%)")
+
 
 # Parameters
 # β (beta) is transmission rate and I chose 30%. γ (gamma) is set to 1%
-beta = 0.7
+beta = 0.3
 gamma = 0.1
 
 # Time vector
